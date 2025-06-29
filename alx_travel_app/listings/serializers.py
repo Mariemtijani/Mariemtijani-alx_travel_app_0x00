b1f6ae33955026f65885f201d9dc4691dbebbe56
Mariem Tijani <mariemtijani60@gmail.com>
2025-06-29 19:47:43 +0100

    seed

--- alx_travel_app/listings/serializers.py
+++ alx_travel_app/listings/serializers.py
@@ -1,13 +1,13 @@
 from rest_framework import serializers
 from .models import Listing, Booking, Review
 
-class BookingSerializer(serializers.ModelSerializer):
+class Booking(serializers.ModelSerializer):
     class Meta:
         model = Booking
         fields = '__all__'
 
-class ListingSerializer(serializers.ModelSerializer):
-    bookings = BookingSerializer(many=True, read_only=True)
+class Listing(serializers.ModelSerializer):
+    bookings = Booking(many=True, read_only=True)
 
     class Meta:
         model = Listing
